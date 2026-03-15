a9c5b3ea1fd9ebdb999981a9a82fcb237e5cc727
Igor da Costa Gouveia <cgouveiaigor@gmail.com>
2026-03-15 15:50:31 -0300

    feat: validações

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -3,33 +3,44 @@ import UserModel from '../models/UserModel';
 class UserController {
 
   async create(req, res) {
+    try{
     const data = req.body;
     const newUser =  await UserModel.create(data);
-
     res.status(201).json({name:newUser.name,email:newUser.email,role:newUser.role});
-
+  }catch(err){
+    if( err.name === 'SequelizeUniqueConstraintError'){
+      return res.status(409).json({
+        errors:['Email já cadastrado'],
+      });
+    }
+    res.status(400).json({errors:err.errors.map(er=>er.message)});
+    }
   }
 
   async read(req, res) {
     const users = await UserModel.findAll({attributes: ['id', 'name', 'email', 'role']});
-
-
     res.status(200).json(users);
   }
 
   async readOne(req, res){
+    
     const {id} = req.params;
     const user = await UserModel.findByPk(id, {attributes: ['id', 'name', 'email', 'role']});
-
-    res.status(200).json(user)
+    if(!user){
+      return res.status(400).json({message: "Usuário não existe."});
+    }
+    return res.status(200).json(user)
   }
   
   async update (req, res){
-    const {id} = req.params;
-    const user = req.body;
-    await UserModel.update(user, {where: {id}});
-
-    res.status(200).json({name:user.name,email:user.email,role:user.role});
+    try {
+      const {id} = req.params;
+      const user = req.body;
+      await UserModel.update(user, {where: {id}});
+      res.status(200).json({name:user.name,email:user.email,role:user.role});
+    }catch(err){
+      res.status(400).json({errors:err.errors.map(er=>er.message)})
+    }
   }
 
   async delete(req, res){
